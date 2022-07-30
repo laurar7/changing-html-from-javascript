6767f7f224d9ccf65949e0b977b6615fa3c6d851
Laura R <102334514+laurar7@users.noreply.github.com>
2022-07-30 14:21:32 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var firstListItem = document.querySelector("ul li");
+firstListItem.style.textTransform = "uppercase";
+// console.log(firstListItem);
+
+var moreReasons = document.querySelector(".more-reasons");
+moreReasons.style.fontSize = "2.5em";
+// console.log(moreReasons);
+
+var whyJS = document.querySelector("h3");
+whyJS.innerHTML = 'Why learn <span class="highlight">JavaScript?</span>';
+var highlight = document.querySelector(".highlight");
+highlight.style.background = "#ffff82";
+// console.log(whyJS);
+
+var mainImage = document.querySelector("img");
+mainImage.src = "../img/js-code.png";
+mainImage.alt = "JavaScript code example";
+
+// console.log(mainImage);
